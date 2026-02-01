21dbf29f0cb3eeb9db4485533148bb67243a0ecd
Leo Lin <88123479+FroggLeo@users.noreply.github.com>
2026-02-01 15:10:25 +0800

    add level card to level select

--- scenes/level_select/level_card.gd
+++ scenes/level_select/level_card.gd
@@ -7,6 +7,9 @@ func set_image(image: String) -> void:
 	%icon.texture = image
 	%icon.visible = true
 
+func clear_image() -> void:
+	%icon.visible = false
+
 func set_txt(title: String, author: String) -> void:
 	%title.text = title
 	%author.text = author
