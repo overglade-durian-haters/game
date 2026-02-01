b52b02d3f87240fb7a875e32c624564ab6a1b039
Leo Lin <88123479+FroggLeo@users.noreply.github.com>
2026-02-01 14:47:03 +0800

    add level select menu, level item, and level casrd

---
+++ scenes/level_select/level_item.gd
@@ -0,0 +1,12 @@
+extends Button
+
+func _ready() -> void:
+	%icon.visible = false
+
+func set_image(image: String) -> void:
+	%icon.texture = image
+	%icon.visible = true
+
+func set_txt(title: String, author: String) -> void:
+	%title.text = title
+	%author.text = author
